acadca7ef8012ce53d7478da00649a69a888dd37
johny <johnyaov@gmail.com>
2021-04-16 19:46:04 -0500

    subida con css y js corregido

--- js/dist/registro.dev.js
+++ js/dist/registro.dev.js
@@ -13,6 +13,7 @@ function eventListeners() {
   nombre.addEventListener('blur', validarFormulario);
   email.addEventListener('blur', validarFormulario);
   numTelefono.addEventListener('blur', validarFormulario);
+  btnEnviar.addEventListener('click', envairRegistro);
 }
 
 function iniciarAPP() {
@@ -45,28 +46,64 @@ function validarFormulario(e) {
       mostrarMensaje('este Email no es valido');
     }
   }
+
+  if (er.test(email.value) && nombre.value !== '' && numTelefono !== '') {
+    btnEnviar.disabled = false;
+  }
+  /*
+  const diferencia = new Date(). getFullYear() - anio.value;
+  if(diferencia <= 18){
+      mostrarMensaje('debes ser mayor de edad para contactarnos');
+  } else {
+      mostrarMensaje('estas a un paso de contactarnos');
+  }
+  */
+
 }
 
 function mostrarMensaje(mensaje) {
   var mensajeError = document.createElement('p');
   mensajeError.classList.add('input-error', 'error');
   mensajeError.textContent = mensaje;
-  var errores = document.querySelector('.error');
+  var errores = document.querySelector('.error'); //formulario.appendChild(mensajeError);
 
-  if (errores.length >= 0) {
+  if (errores != null) {
     errores.remove();
   } else {
     formulario.appendChild(mensajeError);
   }
 }
 
-var anio = document.querySelector('#anio');
-var max = new Date().getFullYear(),
-    min = max - 30;
+function envairRegistro(e) {
+  e.preventDefault(); //mostrar spiner
 
-for (var i = max; i > min; i--) {
-  var option = document.createElement('option');
-  option.value = i;
-  option.innerHTML = i;
-  anio.appendChild(option);
+  var spiner = document.querySelector('.spiner');
+  spiner.style.display = 'flex'; //despues de 3 segundos ocultar spiner
+
+  setTimeout(function () {
+    spiner.style.display = 'none';
+    var parrafo = document.createElement('p');
+    parrafo.textContent = 'el usuario a sido registrado con exito';
+    parrafo.classList.add('input-correcto', 'envio');
+    formulario.insertBefore(parrafo, spiner);
+    setTimeout(function () {
+      parrafo.remove();
+      formulario.reset();
+      iniciarAPP();
+    }, 3000);
+  }, 3000);
 }
+/*
+const anio = document.querySelector('#anio');
+const max = new Date().getFullYear(),
+    min = max -30;
+
+   
+
+for(let i = max; i > min; i-- ){
+    const option = document.createElement('option');
+    option.value = i;
+    option.innerHTML = i;
+    anio.appendChild(option);
+}
+*/
